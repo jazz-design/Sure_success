eecb2ec758d20ada0e84eed18eddd34969ecba8d
Jazz <102905413+jazz-design@users.noreply.github.com>
2023-09-08 16:48:28 +0530

    updates mailid

--- contact.php
+++ contact.php
@@ -1,88 +1,175 @@
-<?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require "phpmailer/Exception.php";
-require "phpmailer/PHPMailer.php";
-require "phpmailer/SMTP.php";
-
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sanitize and Validate POST data
-    $name = strip_tags($_POST["name"]);
-    $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
-    $option = strip_tags($_POST["option"]);
-    $message = strip_tags($_POST["message"]);
-
-
-    // Check if any field is empty or email is invalid
-    if (empty($name) || empty($email) || empty($option) || empty($message) || !$email) {
-        header("Location: contact-us.php?status=error");
-            exit();
-        exit();
-    }
-
-    // Initialize PHPMailer
-    $mail = new PHPMailer(true);
-
-    try {
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';  // Use your SMTP host
-        $mail->SMTPAuth = true;
-        $mail->Username = 'vichaar.team@gmail.com';  // Use your SMTP username
-        $mail->Password = 'rptxtapxyixchipd';  // Use your SMTP password
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-        // Recipients
-        //$mail->setFrom('vichaar.team@gmail.com');
-        $mail->addAddress('vichaar.team@gmail.com', 'Vichaar.team');  
-
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Contact Form Sure Success';
-
-        // HTML Content
-        $htmlContent = "
-        <html>
-        <head>
-            <title>Contact Form Email</title>
-        </head>
-        <body>
-            <table width='95%' border='1' cellspacing='1' cellpadding='1' align='center'>
-                <tr>
-                    <td><b>Sure Success Contact Enquiry Detail</b></td>
-                </tr>
-                <tr>
-                    <td><b>Name:</b></td><td>{$name}</td>
-                </tr>
-                <tr>
-                    <td><b>Option Choosen:</b></td><td>{$option}</td>
-                </tr>
-                <tr>
-                    <td><b>Email:</b></td><td>{$email}</td>
-                </tr>
-                <tr>
-                    <td><b>Message:</b></td><td>{$message}</td>
-                </tr>
-            </table>
-        </body>
-        </html>";
-
-        $mail->Body = $htmlContent;
-
-
-        // Send email
-        $mail->send();
-            header("Location: contact-us.php?status=success");
-            exit();
-        } catch (Exception $e) {
-            //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;  // Display the error
-            header("Location: contact-us.php?status=error");  // Comment this out temporarily to see the error
-            exit();
-        }
-}
+<?php
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+use PHPMailer\PHPMailer\Exception;
+
+
+
+require "phpmailer/Exception.php";
+
+require "phpmailer/PHPMailer.php";
+
+require "phpmailer/SMTP.php";
+
+
+
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Sanitize and Validate POST data
+
+    $name = strip_tags($_POST["name"]);
+
+    $email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+
+    $option = strip_tags($_POST["option"]);
+
+    $message = strip_tags($_POST["message"]);
+
+
+
+
+
+    // Check if any field is empty or email is invalid
+
+    if (empty($name) || empty($email) || empty($option) || empty($message) || !$email) {
+
+        header("Location: contact-us.php?status=error");
+
+            exit();
+
+        exit();
+
+    }
+
+
+
+    // Initialize PHPMailer
+
+    $mail = new PHPMailer(true);
+
+
+
+    try {
+
+        $mail->SMTPDebug = 0;
+
+        $mail->isSMTP();
+
+        $mail->Host = 'smtp.gmail.com';  // Use your SMTP host
+
+        $mail->SMTPAuth = true;
+
+        $mail->Username = 'suresuccessquery@gmail.com';  // Use your SMTP username
+
+        $mail->Password = 'pejdfhpipgnbtimf';  // Use your SMTP password
+
+        $mail->SMTPSecure = 'tls';
+
+        $mail->Port = 587;
+
+
+
+        // Recipients
+
+        //$mail->setFrom('suresuccessquery@gmail.com');
+
+        $mail->addAddress('suresuccessquery@gmail.com', 'Sure Success');  
+
+
+
+        // Content
+
+        $mail->isHTML(true);
+
+        $mail->Subject = 'Contact Form Sure Success';
+
+
+
+        // HTML Content
+
+        $htmlContent = "
+
+        <html>
+
+        <head>
+
+            <title>Contact Form Email</title>
+
+        </head>
+
+        <body>
+
+            <table width='95%' border='1' cellspacing='1' cellpadding='1' align='center'>
+
+                <tr>
+
+                    <td><b>Sure Success Contact Enquiry Detail</b></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Name:</b></td><td>{$name}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Option Choosen:</b></td><td>{$option}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Email:</b></td><td>{$email}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Message:</b></td><td>{$message}</td>
+
+                </tr>
+
+            </table>
+
+        </body>
+
+        </html>";
+
+
+
+        $mail->Body = $htmlContent;
+
+
+
+
+
+        // Send email
+
+        $mail->send();
+
+            header("Location: contact-us.php?status=success");
+
+            exit();
+
+        } catch (Exception $e) {
+
+            //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;  // Display the error
+
+            header("Location: contact-us.php?status=error");  // Comment this out temporarily to see the error
+
+            exit();
+
+        }
+
+}
+
 ?>
--- queryform.php
+++ queryform.php
@@ -1,100 +1,200 @@
-<?php
-
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require "phpmailer/Exception.php";
-require "phpmailer/PHPMailer.php";
-require "phpmailer/SMTP.php";
-
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    
-    // Collecting POST data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $enquiry = $_POST["enquiry"];
-    $phone = $_POST["phone"];
-    $country = $_POST["country"];
-    $ilets = $_POST["ilets"];
-    $refusal = $_POST["refusal"];
-
-    // Check if any field is empty or email is invalid
-    if (empty($name) || empty($email) || empty($enquiry) || empty($phone) || !$email) {
-        header("Location: query.php?status=error");
-            exit();
-        exit();
-    }
-
-    // Initialize PHPMailer
-    $mail = new PHPMailer(true);
-
-    try {
-        $mail->SMTPDebug = 0;
-        $mail->isSMTP();
-        $mail->Host = 'smtp.gmail.com';  // Use your SMTP host
-        $mail->SMTPAuth = true;
-        $mail->Username = 'vichaar.team@gmail.com';  // Use your SMTP username
-        $mail->Password = 'rptxtapxyixchipd';  // Use your SMTP password
-        $mail->SMTPSecure = 'tls';
-        $mail->Port = 587;
-
-        // Recipients
-        //$mail->setFrom('vichaar.team@gmail.com');
-        $mail->addAddress('vichaar.team@gmail.com', 'Vichaar.team');  
-
-        // Content
-        $mail->isHTML(true);
-        $mail->Subject = 'Enquiry Form Sure Success';
-
-        // HTML Content
-        $htmlContent = "
-        <html>
-        <head>
-            <title>Contact Form Email</title>
-        </head>
-        <body>
-            <table width='95%' border='1' cellspacing='1' cellpadding='1' align='center'>
-                <tr>
-                    <td><b>Sure Success Contact Enquiry Detail</b></td>
-                </tr>
-                <tr>
-                    <td><b>Name:</b></td><td>{$name}</td>
-                </tr>
-                <tr>
-                    <td><b>Enquiry About:</b></td><td>{$enquiry}</td>
-                </tr>
-                <tr>
-                    <td><b>Email:</b></td><td>{$email}</td>
-                </tr>
-                <tr>
-                    <td><b>Phone number:</b></td><td>{$phone}</td>
-                </tr>
-                <tr>
-                    <td><b>Country:</b></td><td>{$country}</td>
-                </tr>
-                <tr>
-                    <td><b>ILETS:</b></td><td>{$ilets}</td>
-                </tr>
-                <tr>
-                    <td><b>Any Refusal of Visa:</b></td><td>{$refusal}</td>
-                </tr>
-            </table>
-        </body>
-        </html>";
-
-        $mail->Body = $htmlContent;
-
-
-        // Send email
-        $mail->send();
-            header("Location: query.php?status=success");
-            exit();
-        } catch (Exception $e) {
-            //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;  // Display the error
-            header("Location: query.php?status=error");  // Comment this out temporarily to see the error
-            exit();
-        }
-}
-?>
+<?php
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+use PHPMailer\PHPMailer\Exception;
+
+
+
+require "phpmailer/Exception.php";
+
+require "phpmailer/PHPMailer.php";
+
+require "phpmailer/SMTP.php";
+
+
+
+
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    
+
+    // Collecting POST data
+
+    $name = $_POST["name"];
+
+    $email = $_POST["email"];
+
+    $enquiry = $_POST["enquiry"];
+
+    $phone = $_POST["phone"];
+
+    $country = $_POST["country"];
+
+    $ilets = $_POST["ilets"];
+
+    $refusal = $_POST["refusal"];
+
+
+
+    // Check if any field is empty or email is invalid
+
+    if (empty($name) || empty($email) || empty($enquiry) || empty($phone) || !$email) {
+
+        header("Location: query.php?status=error");
+
+            exit();
+
+        exit();
+
+    }
+
+
+
+    // Initialize PHPMailer
+
+    $mail = new PHPMailer(true);
+
+
+
+    try {
+
+        $mail->SMTPDebug = 0;
+
+        $mail->isSMTP();
+
+        $mail->Host = 'smtp.gmail.com';  // Use your SMTP host
+
+        $mail->SMTPAuth = true;
+
+        $mail->Username = 'suresuccessquery@gmail.com';  // Use your SMTP username
+
+        $mail->Password = 'pejdfhpipgnbtimf';  // Use your SMTP password
+
+        $mail->SMTPSecure = 'tls';
+
+        $mail->Port = 587;
+
+
+
+        // Recipients
+
+        //$mail->setFrom('suresuccessquery@gmail.com');
+
+        $mail->addAddress('suresuccessquery@gmail.com', 'Sure Success');  
+
+
+
+        // Content
+
+        $mail->isHTML(true);
+
+        $mail->Subject = 'Enquiry Form Sure Success';
+
+
+
+        // HTML Content
+
+        $htmlContent = "
+
+        <html>
+
+        <head>
+
+            <title>Contact Form Email</title>
+
+        </head>
+
+        <body>
+
+            <table width='95%' border='1' cellspacing='1' cellpadding='1' align='center'>
+
+                <tr>
+
+                    <td><b>Sure Success Contact Enquiry Detail</b></td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Name:</b></td><td>{$name}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Enquiry About:</b></td><td>{$enquiry}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Email:</b></td><td>{$email}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Phone number:</b></td><td>{$phone}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Country:</b></td><td>{$country}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>ILETS:</b></td><td>{$ilets}</td>
+
+                </tr>
+
+                <tr>
+
+                    <td><b>Any Refusal of Visa:</b></td><td>{$refusal}</td>
+
+                </tr>
+
+            </table>
+
+        </body>
+
+        </html>";
+
+
+
+        $mail->Body = $htmlContent;
+
+
+
+
+
+        // Send email
+
+        $mail->send();
+
+            header("Location: query.php?status=success");
+
+            exit();
+
+        } catch (Exception $e) {
+
+            //echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;  // Display the error
+
+            header("Location: query.php?status=error");  // Comment this out temporarily to see the error
+
+            exit();
+
+        }
+
+}
+
+?>
+
